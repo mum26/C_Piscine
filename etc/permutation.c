08883008893a34872f501e2b5e5d2f3cb9c928af
石毛秀悟 <ishigeshuugo@shuugoea.local>
2024-02-18 00:53:57 +0900

    macbook 2nd

--- etc/permutation.c
+++ etc/permutation.c
@@ -13,47 +13,6 @@
 #include <stdio.h>
 #include <unistd.h>
 
-void	swap(int *a, int *b)
-{
-	int temp;
-
-	temp = *a;
-	*a = *b;
-	*b = temp;
-}
-
-void	reverse(int array[], int start, int end)
-{
-	while(start < end)
-	{
-		swap(&array[start], &array[end]);
-		start++;
-		end--;
-	}
-}
-
-int	next_permutation(int array[], int size)
-{
-	int	i;
-	int	j;
-
-	i = size - 2;
-	while(i >= 0 && array[i] >= array[i + 1])
-		i--;
-
-	if (i < 0)
-		return (0);
-
-	j = size - 1;
-	while(array[j] <= array[i])
-		j--;
-	swap(&array[i], &array[j]);
-
-	reverse(array, i + 1, size - 1);
-
-	return 1;
-}
-
 void	print_array(int *array, int size)
 {
 	int a;
@@ -65,20 +24,43 @@ void	print_array(int *array, int size)
 	write(1, &a, 1);
 }
 
+void	swap(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void	reverse(int *array, int start, int end)
+{
+	while(start < end)
+	{
+		swap(&array[start], &array[end]);
+		start++;
+		end--;
+	}
+}
+
+void generate_permutations(int *array, int size, int start, int end) {
+    if (start == end) {
+	print_array(array, size);
+        write(1, "\n", 1);
+    } else {
+        for (int i = start; i <= end; i++) {
+            swap(&array[start], &array[i]);
+            generate_permutations(array, size, start + 1, end);
+            swap(&array[start], &array[i]); // 元に戻す
+        }
+    }
+}
+
 int	main(void)
 {
 	int	array[] = {1, 2, 3, 4};
 	int	size = 4;
-	int	has_next;
 
-	has_next = 1;
-	print_array(array, size);
-	printf("\n");
-	
-	while(next_permutation(array, size))
-	{
-		print_array(array, size);
-		write(1, "\n", 1);
-	}
+	generate_permutations(array, size, 0, size - 1);
 	return (0);
 }
