f63f2acad56adb43cf24b5a0edc545d2767d79f5
石毛秀悟 <ishigeshuugo@shuugoea.local>
2024-02-20 11:18:16 +0900

    2nd commit

---
+++ c03/ex01/ft_strncmp.c
@@ -0,0 +1,25 @@
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (i < n && s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
+	{
+		i++;
+	}
+	if (i < n)
+		return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	return (0);
+}
+
+/*
+#include <stdio.h>
+int	main(void)
+{
+	printf("(abc, abc) = %d\n", ft_strncmp("abc", "abc", 2)); // 返り値は 0
+	printf("(abc, abe) = %d\n", ft_strncmp("abc", "abe", 3)); // 負の整数を返す
+	printf("(abd, abc) = %d\n", ft_strncmp("abd", "abc", 3)); // 正の整数を返す
+	printf("(abc, ab) = %d\n", ft_strncmp("abc", "ab", 4));  // 正の整数を返す
+	return (0);
+}
+*/
