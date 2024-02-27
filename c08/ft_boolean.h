b9459201d89eda01868ef85de4c35ce6f80a9bd0
石毛秀悟 <ishigeshuugo@shuugoea.local>
2024-02-28 01:39:28 +0900

    test

---
+++ c08/ft_boolean.h
@@ -0,0 +1,15 @@
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+
+# define TRUE 1
+# define FALSE 0
+# define SUCCESS 0
+# define EVEN(x) ((x) % 2 == 0)
+# define EVEN_MSG "I have an even number of arguments."
+# define ODD_MSG "I have an odd number of arguments."
+
+# include <unistd.h>
+
+typedef int	t_bool;
+
+#endif
